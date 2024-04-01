7788b1bc9dfb1e36233b1290e831585c61bd69ee
MyJetson <adamfong888@gmail.com>
2024-04-02 00:40:27 +0200

    Adding multiple cylinder spawns, hard coding the rotation to make the spawned cylinders level compared to base_link

--- src/cluster_extractor.cpp
+++ src/cluster_extractor.cpp
@@ -4,7 +4,10 @@
 #include <ros/ros.h>
 #include <Eigen/Dense>
 #include <tf/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
 #include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/TransformStamped.h>
 
 #include <pcl/ModelCoefficients.h>
 #include <pcl/point_types.h>
@@ -18,6 +21,8 @@
 // for added pcl::PointCloud ros message function
 #include <pcl_ros/point_cloud.h>
 
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit_msgs/CollisionObject.h>
 
 //---------------------------------------------------------------------------
 // Global Variables
@@ -41,13 +46,16 @@ private:
     ros::Publisher cloud_pub[MAX_CLUSTERS];
     ros::Publisher point_pub[MAX_CLUSTERS];
     tf::TransformBroadcaster br;
-
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
+        // tf2_ros::Buffer tfBuffer;
+        // tf2_ros::TransformListener tfListener(tfBuffer);
 
 public:
+   
     ClusterExtractor()
     {
-        ROS_DEBUG("Creating subscribers and publishers");
         cloud_sub = n_.subscribe("/outlier/cutoff/output", 10, &ClusterExtractor::cloudcb, this);
+        ROS_DEBUG("Creating subscribers and publishers");
         br = tf::TransformBroadcaster();
         for(int i = 0; i < MAX_CLUSTERS; i++)
         {
@@ -56,9 +64,73 @@ public:
         }
     }
 
+    moveit_msgs::CollisionObject addCylinder(float x, float y, float z, std::string id)
+    {
+        // BEGIN_SUB_TUTORIAL add_cylinder
+        //
+        // Adding Cylinder to Planning Scene
+        // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        // Define a collision object ROS message.
+        
+        // geometry_msgs::TransformStamped transformStamped;
+        // try{
+        //     transformStamped = tfBuffer.lookupTransform("base_link", "camera_depth_optical_frame", ros::Time(0));
+        // }
+        // catch (tf2::TransformException &ex){
+        //     ROS_WARN("%s", ex.what());
+        //     ros::Duration(1.0).sleep();
+        // }
+
+        moveit_msgs::CollisionObject collision_object;
+        collision_object.header.frame_id = "camera_depth_optical_frame";
+        collision_object.id = id;
+
+        // Define a cylinder which will be added to the world.
+        shape_msgs::SolidPrimitive primitive;
+        primitive.type = primitive.CYLINDER;
+        primitive.dimensions.resize(2);
+        /* Setting height of cylinder. */
+        primitive.dimensions[0] = 0.1;
+        /* Setting radius of cylinder. */
+        primitive.dimensions[1] = 0.1;
+
+        // Define a pose for the cylinder (specified relative to frame_id).
+        geometry_msgs::Pose cylinder_pose;
+        /* Computing and setting quaternion from axis angle representation. */
+        Eigen::Vector3d cylinder_z_direction(0., 0., 0.);
+        Eigen::Vector3d origin_z_direction(0., 0., 1.);
+        Eigen::Vector3d axis;
+        axis = origin_z_direction.cross(cylinder_z_direction);
+        axis.normalize();
+        double angle = acos(cylinder_z_direction.dot(origin_z_direction));
+        cylinder_pose.orientation.x = 3.35;
+        cylinder_pose.orientation.y = axis.y() * sin(angle / 2);
+        cylinder_pose.orientation.z = axis.z() * sin(angle / 2);
+        cylinder_pose.orientation.w = cos(angle / 2);
+        // cylinder_pose.orientation.x = transformStamped.transform.rotation.x;
+        // cylinder_pose.orientation.y = transformStamped.transform.rotation.y;
+        // cylinder_pose.orientation.z = transformStamped.transform.rotation.z;
+        // cylinder_pose.orientation.w = transformStamped.transform.rotation.w;
+
+        // Setting the position of cylinder.
+        cylinder_pose.position.x = x;
+        cylinder_pose.position.y = y;
+        cylinder_pose.position.z = z;
+
+        // Add cylinder as collision object
+        collision_object.primitives.push_back(primitive);
+        collision_object.primitive_poses.push_back(cylinder_pose);
+        collision_object.operation = collision_object.ADD;
+        // collision_objects.push_back(collision_object);
+        // planning_scene_interface_.applyCollisionObject(collision_object);
+        // END_SUB_TUTORIAL
+        return collision_object;
+    }
+
     // this function gets called every time new pcl data comes in
     void cloudcb(const sensor_msgs::PointCloud2ConstPtr &scan)
     {
+        
         ROS_DEBUG("Filtered cloud receieved");
         PointCloud::Ptr ros_cloud(new PointCloud);
         PointCloud::Ptr cloud (new PointCloud);
@@ -93,12 +165,15 @@ public:
         int number_clusters=0;
         geometry_msgs::PointStamped pt;
         Eigen::Vector4f centroid;
+        std::vector<moveit_msgs::CollisionObject> collision_objects;
+        collision_objects.resize(5);
 
         for(const auto & index : cluster_indices)
         {
             number_clusters = (int) cluster_indices.size();
             ROS_DEBUG("Number of clusters found: %d",number_clusters);
             PointCloud::Ptr cloud_cluster (new PointCloud);
+
             for (const auto & point : index.indices)
             {
                 cloud_cluster->points.push_back(cloud->points[point]);
@@ -107,7 +182,6 @@ public:
             cloud_cluster->height = 1;
             cloud_cluster->is_dense = true;
             cloud_cluster->header.frame_id = "camera_depth_optical_frame";
-
 
             // convert to rosmsg and publish:
             ROS_DEBUG("Publishing extracted cloud");
@@ -134,9 +208,14 @@ public:
                 q.setRPY(0, 0, 0);
                 transform.setRotation(q);
                 br.sendTransform( tf::StampedTransform(transform, ros::Time::now(), scan->header.frame_id, "cluster_" + std::to_string(j + 1) + "_frame"));
+            
+                std::string id = "cylinder_" + std::to_string(j + 1);
+                collision_objects.push_back(addCylinder(centroid(0), centroid(1), centroid(2), id));
             }
             j++;
+        // ros::Duration(2.0).sleep();
         }
+        planning_scene_interface_.applyCollisionObjects(collision_objects);
     }
 };
 
@@ -144,6 +223,7 @@ public:
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "cluster_extractor");
+    
     ClusterExtractor extractor;
   
     ros::spin();
